2a8744c4d1631510b244032797b75cd9ed745f51
jeremy302 <olajeremy123@gmail.com>
2023-04-25 19:13:26 +0100

    patch

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,28 +1,28 @@
-#!/usr/bin/python3
-''' <TODO> add documentation '''
-from flask import Flask, render_template
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
 class Config:
     ''' config class '''
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-app.config['LANGUAGES'] = (Config.LANGUAGES)
-app.config['BABEL_DEFAULT_LOCALE'] = (Config.BABEL_DEFAULT_LOCALE)
-app.config['BABEL_DEFAULT_TIMEZONE'] = (Config.BABEL_DEFAULT_TIMEZONE)
-
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index() -> str:
-    ''' index page '''
+    ''' index route '''
     return render_template('1-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
