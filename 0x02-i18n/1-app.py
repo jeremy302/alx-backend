4cc10ce586f36aed1e8169badce401fd5cf7dd4c
jeremy302 <olajeremy123@gmail.com>
2023-04-25 19:08:41 +0100

    patch

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,7 +5,8 @@ from flask_babel import Babel
 
 
 class Config:
-    ''' config class doc '''
+    """config class for the flask app
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
