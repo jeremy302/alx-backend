b6e0f301153fc025602738b15e97aa70d63871b3
jeremy302 <olajeremy123@gmail.com>
2023-04-25 18:54:54 +0100

    patch

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 ''' <TODO> add documentation '''
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def get_index() -> str:
+@app.route('/', strict_slashes=False)
+def index() -> str:
     ''' index page '''
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
