aa36d0ab4bae5f0d2d57c70a32fd4e31dc3082f0
jeremy302 <olajeremy123@gmail.com>
2023-04-25 18:46:48 +0100

    patch

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python3
-''' <TODO> add documentation '''
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    ''' index page '''
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
