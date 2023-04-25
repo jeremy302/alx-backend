d223362f9947d7afc87af51a0678e2c1d09b0afe
jeremy302 <olajeremy123@gmail.com>
2023-04-25 19:28:59 +0100

    patch

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
-"""
-from flask_babel import Babel
+''' <TODO> add documentation '''
 from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
 class Config:
-    """Represents a Flask Babel configuration.
-    """
+    ''' config class '''
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
@@ -21,17 +19,18 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    ''' gets locale '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
+def index() -> str:
+    ''' index page '''
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
